d4929e4885ceec553f8f8538339f38a23f947691
Faiz Mohammed Khan <faizmohammedkhan7@gmail.com>
2023-12-17 19:12:20 +0100

    Adding the Web2py application

---
+++ controllers/default.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# This is a sample controller
+# this file is released under public domain and you can use without limitations
+# -------------------------------------------------------------------------
+
+# ---- example index page ----
+def index():
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+# ---- API (example) -----
+@auth.requires_login()
+def api_get_user_email():
+    if not request.env.request_method == 'GET': raise HTTP(403)
+    return response.json({'status':'success', 'email':auth.user.email})
+
+# ---- Smart Grid (example) -----
+@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+def grid():
+    response.view = 'generic.html' # use a generic view
+    tablename = request.args(0)
+    if not tablename in db.tables: raise HTTP(403)
+    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    return dict(grid=grid)
+
+# ---- Embedded wiki (example) ----
+def wiki():
+    auth.wikimenu() # add the wiki to the menu
+    return auth.wiki() 
+
+# ---- Action for login/register/etc (required for auth) -----
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+# ---- action to server uploaded static content (required) ---
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def test_view():
+    values = request.vars
+    myText = 'Hello Project 3, Access Platform for providers'
+    form = SQLFORM(db.university)
+    if form.process().accepted:
+        response.flash = 'Your form was submitted successfully'
+    else:
+        response.flash = 'there is an error'
+
+    form_2 = SQLFORM.factory(
+        Field('name', 'string', required=IS_NOT_EMPTY()),
+        Field('ranking', 'integer')
+    )
+    if form_2.process().accepted:
+        db.university.insert(name=form_2.vars.name, ranking=form_2.vars.ranking)
+    new_value = values.para
+    return dict(myText=myText, form_2=form_2, new_value=new_value)
+
+
+def edit_provider_credentials():
+    credential_form = SQLFORM.factory(
+        Field('provider_name', 'string'),
+        Field('email', 'string'),
+        Field('password', 'string'),
+        Field('re_enter_password', 'string'),
+        Field('role', 'string'), submit_button='Sign up'
+    )
+
+    return dict(credential_form=credential_form)
+
+
+def provider_admin():
+    total_users = 8
+    providers = 3
+    admin = 4
+    roles = 3
+
+    return dict(total_users=total_users, providers=providers, admin=admin, roles=roles)
