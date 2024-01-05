36d91df823d54da3807e1131242dbcd717fd05e5
Faiz Mohammed Khan <faizmohammedkhan7@gmail.com>
2024-01-05 22:02:43 +0100

    fix name

--- controllers/api.py
+++ controllers/api.py
@@ -1,7 +1,7 @@
 import json
 from collections import defaultdict
 
-def get_g4a_data():
+def get_g3a_data():
     main_dict = defaultdict()
     rows = db((db.p_user.Role=='SuperAdmin') | (db.p_user.Role=='Admin')).select()
     user_list = [{'username':row.Username, 'first_name':row.first_name, 'last_name':row.last_name,
