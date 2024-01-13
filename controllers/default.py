d2d86f0f73ef222b4514fe553c5454ce37fd9caf
Singari Sahith Kumar <sahithkumar1999@gmail.com>
2024-01-13 15:22:05 +0100

    Pages update

--- controllers/default.py
+++ controllers/default.py
@@ -1,24 +1,61 @@
 # -*- coding: utf-8 -*-
-from collections import defaultdict
 # -------------------------------------------------------------------------
 # This is a sample controller
 # this file is released under public domain and you can use without limitations
 # -------------------------------------------------------------------------
-import datetime
-import sys
-sys.path.append('/modules')
 
-# Now you can import your module
-from acl import Access
-
-
-
-
-access=Access(db, session)
-Access.buildAccessCache(db)
 # ---- example index page ----
 def index():
-    redirect(URL('WelCome'))
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+# ---- API (example) -----
+@auth.requires_login()
+def api_get_user_email():
+    if not request.env.request_method == 'GET': raise HTTP(403)
+    return response.json({'status':'success', 'email':auth.user.email})
+
+# ---- Smart Grid (example) -----
+@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+def grid():
+    response.view = 'generic.html' # use a generic view
+    tablename = request.args(0)
+    if not tablename in db.tables: raise HTTP(403)
+    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    return dict(grid=grid)
+
+# ---- Embedded wiki (example) ----
+def wiki():
+    auth.wikimenu() # add the wiki to the menu
+    return auth.wiki() 
+
+# ---- Action for login/register/etc (required for auth) -----
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+# ---- action to server uploaded static content (required) ---
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
 
 
 def test_view():
@@ -59,137 +96,3 @@ def provider_admin():
     roles = 3
 
     return dict(total_users=total_users, providers=providers, admin=admin, roles=roles)
-
-def Team():
-
-    return dict()
-
-def About():
-    return dict()
-
-def team():
-    response.view = 'default/Base_FrameWork.html'
-    return dict()
-
-
-def WelCome():
-    return dict()
-
-def login():
-    # Your login logic goes here
-    form = SQLFORM.factory(
-        Field('Email', 'string', requires=IS_NOT_EMPTY()),
-        Field('password', 'password', requires=IS_NOT_EMPTY())
-    )
-
-    if form.process().accepted:
-        # Example: Check credentials against a database
-        user = db(db.p_user.Email == form.vars.Email).select().first()
-        if user and user.Password == form.vars.password:
-            session.username = user.Email  # Use 'Email' instead of 'Username'
-            user.update_record(LastLoginDate=datetime.datetime.now())
-            redirect(URL('user_dashboard'))  # Redirect to dashboard after successful login
-        response.flash = 'Invalid credentials'
-
-    return dict(form=form)
-
-
-def logout():
-    session.username=None
-    redirect(URL('WelCome'))
-
-def user_dashboard():
-    userid = request.vars.userid
-    user_record=None
-    if userid:
-        user_record = db(db.p_user.id==userid).select().first()
-    if not access.is_superAdmin():
-        roles_array = ['Admin', 'BasicUser']
-        user_rows = db(db.p_user.Role!='SuperAdmin').select()
-    else:
-        roles_array = ['SuperAdmin', 'Admin', 'BasicUser']
-        user_rows = db(db.p_user).select()
-    fields = [db.p_user.Username, db.p_user.first_name, db.p_user.last_name, db.p_user.Email, db.p_user.Role, db.p_user.RegistrationDate, db.p_user.ma_id, db.p_user.LastLoginDate]
-    if access.is_superAdmin():
-        grid = SQLFORM.grid(db.p_user, user_signature=False, fields=fields)
-    elif access.isAdmin():
-        grid = SQLFORM.grid((db.p_user.Role=='Admin') | (db.p_user.Role=='BasicUser'), user_signature=False, fields=fields)
-    else:
-        redirect(URL('domain'))
-    super_admin_count = db(db.p_user.Role=='SuperAdmin').count()
-    admin_count = db(db.p_user.Role=='Admin').count()
-    basic_user_count = db(db.p_user.Role=='BasicUser').count()
-    form = SQLFORM.factory(
-        Field('Username', 'string', requires=IS_NOT_EMPTY()),
-        Field('first_name', 'string', requires=IS_NOT_EMPTY()),
-        Field('last_name', 'string'),
-        Field('Email', 'string', requires=IS_NOT_EMPTY()),
-        Field('ma_id', requires=IS_IN_SET([])),
-        Field('Role', requires=IS_IN_SET(roles_array)),
-        Field('Password', 'password', requires=IS_NOT_EMPTY()),
-        record=user_record
-    )
-    if form.process().accepted:
-        if user_record:
-            user_record.update_record(**form.vars)
-            response.flash = 'Record Updated'
-        else:
-            db.p_user.insert(**form.vars)
-            response.flash = 'New Record Created'
-        redirect(URL('user_dashboard'))
-
-    return dict(form=form, grid=grid, super_admin_count=super_admin_count, admin_count=admin_count,
-                basic_user_count=basic_user_count, access=access, user_rows=user_rows)
-
-def edit_user():
-    userid = request.vars.userid
-    user_record = None
-    if userid:
-        user_record = db(db.p_user.id == userid).select().first()
-    if not access.is_superAdmin():
-        roles_array = ['Admin', 'BasicUser']
-    else:
-        roles_array = ['SuperAdmin', 'Admin', 'BasicUser']
-    form = SQLFORM.factory(
-        Field('Username', 'string', requires=IS_NOT_EMPTY()),
-        Field('first_name', 'string', requires=IS_NOT_EMPTY()),
-        Field('last_name', 'string'),
-        Field('Email', 'string', requires=IS_NOT_EMPTY()),
-        Field('ma_id', requires=IS_IN_SET([])),
-        Field('Role', requires=IS_IN_SET(roles_array)),
-        Field('Password', 'password', requires=IS_NOT_EMPTY()),
-        record=user_record
-    )
-    if form.process().accepted:
-        if user_record:
-            user_record.update_record(**form.vars)
-            response.flash = 'Record Updated'
-        else:
-            db.p_user.insert(**form.vars)
-            response.flash = 'New Record Created'
-        redirect(URL('user_dashboard'))
-
-    return dict(form=form)
-
-def delete_user():
-    userid = request.vars.userid
-    db(db.p_user.id==userid).delete()
-    redirect(URL('user_dashboard'))
-
-def domain():
-    ma_rows = db(db.masteragreementtype).select()
-    provider_count = db(db.provider).count()
-    open_ma = db(db.masteragreementtype.ValidUntil>datetime.date.today()).count()
-    closed_ma = db(db.masteragreementtype.ValidUntil< datetime.date.today()).count()
-    return dict(ma_rows=ma_rows, provider_count=provider_count, open_ma=open_ma, closed_ma=closed_ma)
-
-
-def positions():
-    return dict()
-
-def providerpage():
-    response.view='default/provider_layout.html'
-    return dict()
-
-def Forgot_Password():
-    return dict()
